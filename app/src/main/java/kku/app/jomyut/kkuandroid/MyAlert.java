639cb117dcff48e9217aa8f922abcbb25eca5c62
WuddyS <jomyut7@gmail.com>
2016-11-12 15:26:02 +0700

    Initial commit

---
+++ app/src/main/java/kku/app/jomyut/kkuandroid/MyAlert.java
@@ -0,0 +1,40 @@
+package kku.app.jomyut.kkuandroid;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by jomyut on 11/12/2016.
+ */
+
+public class MyAlert {
+
+    private Context context;
+    private int anInt;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, int anInt, String titleString, String messageString) {
+        this.context = context;
+        this.anInt = anInt;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }
+
+    public void myDiaLog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(anInt);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }   //myDialog
+}   //Main Class
+
